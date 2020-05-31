f2cc153cbcd33f0f53293d2b351dbe491464acaf
matteo.abbruzzo <abbruzzomatteo@gmail.com>
2020-05-31 16:44:49 +0200

    Types and constants
    Definge all types and constants for app

--- src/utils/type.ts
+++ src/utils/type.ts
@@ -1,4 +1,30 @@
+export enum Action {
+  Create,
+  Update,
+  Remove,
+}
+
 export enum TileMode {
   create,
   modify,
 }
+
+export type Todo = {
+  Id: number;
+  Name: string;
+  Description: string;
+  CreationDate: string;
+};
+
+export type Recording = {
+  todoItem: Todo;
+  action: Action;
+};
+
+export type RecordedAction = {
+  date: Date;
+  todoId: number;
+};
+
+export const todoDBName = 'todoDB';
+export const todoStoreName = 'recordingStore';
